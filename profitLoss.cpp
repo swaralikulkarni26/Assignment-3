02ddba2eb738efb3fa60805d342ffc8d60313661
swaralikulkarni26 <94820625+swaralikulkarni26@users.noreply.github.com>
2022-11-01 17:47:13 +0530

    Que 6 added

---
+++ profitLoss.cpp
@@ -0,0 +1,26 @@
+//Que 5. Accept cost price(CP) and Selling price(SP) of an item. 
+//Calculate the ammount of PROFIT/LOSS made by the seller and print the 
+#include<iostream>
+using namespace std;
+
+int main(){
+    float cp,sp,loss, profit;
+    cout<<"Enter Cost price: "<<endl;
+    cin>>cp;
+    cout<<"Enter Seling Price: "<<endl;
+    cin>>sp;
+
+    if(sp>cp){
+
+        profit=sp-cp;
+        cout<<"Profit is : "<<profit<<endl;
+    }
+    else if(sp<cp){
+        loss=cp-sp
+        cout<<"Loss is : "<<loss<<endl;
+    }
+    else 
+    cout<<"There is neither profit nor loss."<<endl;
+
+        return 0;
+}
